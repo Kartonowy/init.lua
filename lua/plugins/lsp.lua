222669d891bcadcd2ceba201a66c3494466acd66
Kartonowy <ttrollface.com.pl@gmail.com>
2025-01-23 18:48:04 +0100

    changes

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,48 @@
+return {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            'saghen/blink.cmp',
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim'
+        },
+
+        -- example using `opts` for defining servers
+        opts = {
+            servers = {
+                lua_ls = {},
+                rust_analyzer = {},
+                gopls = {},
+                templ = {},
+                cssls = {},
+                html = {
+                    filetypes = {'html', 'templ'}
+                },
+                htmx = {
+                    filetypes = {'html', 'templ'}
+                },
+                emmet_ls = {
+                    filetypes = {'html', 'templ'}
+                },
+                tailwindcss = {},
+                clangd = {}
+            }
+        },
+        config = function(_, opts)
+            local lspconfig = require('lspconfig')
+            for server, config in pairs(opts.servers) do
+                -- passing config.capabilities to blink.cmp merges with the capabilities in your
+                -- `opts[server].capabilities, if you've defined it
+                config.on_attach = on_attach
+                config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+                config.diagnostics = { enable = false }
+                lspconfig[server].setup(config)
+            end
+            require("mason").setup()
+            require("mason-lspconfig").setup()
+        end
+
+    }
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,16 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate"
+        build = ":TSUpdate",
+        config = function ()
+            local configs = require("nvim-treesitter.configs")
+
+            configs.setup({
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "go" },
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
     }
 }
