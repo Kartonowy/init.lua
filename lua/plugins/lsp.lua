80713e3c7f2c093b382426ac24b1c2bbe2395a0a
Kartonowy <ttrollface.com.pl@gmail.com>
2025-03-27 14:42:41 +0100

    add autopairs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -64,5 +64,12 @@ return {
         end
 
     },
-'github/copilot.vim'
+'github/copilot.vim',
+{
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+}
 }
